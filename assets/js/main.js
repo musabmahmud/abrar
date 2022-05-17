b095f5ee5ec3e16f6426b236c6368a1c4a07308b
musab mahmud <md.musabmahmud100@gmail.com>
2022-05-18 01:14:04 +0600

    client chagned finally done

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,6 @@
         jQuery(".preloader").delay(100).fadeOut("slow");
     });
 
-
     /*----------------------------
     3. Mobile Menu Activation
     -----------------------------*/
@@ -43,83 +42,16 @@
     ------------------------------ */
     $('.header-slider').owlCarousel({
         items: 1,
-        animateOut: 'slideOutLeft',
-        // animateIn: 'flipInX',
-        autoPlay: true,
-        loop: true,
-        navSpeed: 1000,
-        nav: true,
-        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-    });
-
-
-    //Testimonial-slider
-    var teamCarousel = $('.testimonial-slider');
-    if (teamCarousel.length) {
-        teamCarousel.owlCarousel({
-            items: 4,
-            autoPlay: true,
-            loop: true,
-            margin: 20,
-            navSpeed: 1500,
-            nav: false,
-            dots: false,
-            // navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-            responsive: {
-                0: {
-                    items: 1,
-                },
-                576: {
-                    items: 2
-                },
-                768: {
-                    items: 3
-                },
-                992: {
-                    items: 4
-                }
-            }
-        });
-    };
-    //testimonial Area
-    var owl = $('.custom-testi');
-    owl.owlCarousel({
-        loop: true,
         autoplay: true,
-        autoplayTimeout: 2000,
+        animateOut: 'slideOutLeft',
+        autoplayTimeout: 3000,
         autoplayHoverPause: true,
+        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        loop: true,
+        navSpeed: 200,
         nav: true,
-        items: 1,
-        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
     });
 
-
-    //Blog Area
-    var teamCarousel = $('.custom-blog');
-    if (teamCarousel.length) {
-        teamCarousel.owlCarousel({
-            items: 4,
-            autoPlay: true,
-            loop: true,
-            margin: 30,
-            navSpeed: 1500,
-            nav: true,
-            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-            responsive: {
-                0: {
-                    items: 1,
-                    nav: true
-                },
-                768: {
-                    items: 2,
-                    nav: true
-                },
-                992: {
-                    items: 3
-                }
-            }
-        });
-    };
 
     /*----------------------------
     8. magnific Popup active
@@ -129,23 +61,6 @@
         margin: 10
     });
 
-    $('').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        closeOnContentClick: true,
-        closeBtnInside: true,
-        gallery: {
-            enabled: true
-        },
-        zoom: {
-            enabled: true,
-            duration: 300, // don't foget to change the duration also in CSS
-            opener: function (element) {
-                return element.find('img');
-            }
-        }
-    });
-    
     $('.gallery').magnificPopup({
         type: 'image',
         mainClass: 'mfp-with-zoom', // this class is for CSS animation below
@@ -167,7 +82,6 @@
     -------------------------
     11. fadein
     ---------------------------- */
-
 
     new WOW().init();
 
